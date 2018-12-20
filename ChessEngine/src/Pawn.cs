4cd2c4da8a6fb8f2d116869c3e1ef8c1f5881f3b
Justin Leaming <jtleaming@hotmail.com>
2018-12-20 16:37:53 -0700

    Adding another pawn test.

--- ChessEngine/src/Pawn.cs
+++ ChessEngine/src/Pawn.cs
@@ -11,16 +11,21 @@ namespace ChessEngine
         {
             base.Square = currentSquare;
         }
-
+        private bool firstMove = true;
+        public bool FirstMove { get => firstMove; }
 
         public override void Move(ISquare newSquare)
         {
-            if((newSquare.Position.rank != base.Square.Position.rank)
-                || (Convert.ToInt32(newSquare.Position.file) < Convert.ToInt32(base.Square.Position.file)) )
+            if ((newSquare.Position.rank != base.Square.Position.rank)
+            || (Convert.ToInt32(newSquare.Position.file) < Convert.ToInt32(base.Square.Position.file))
+            || (!firstMove && Convert.ToInt32(newSquare.Position.file) > 1))
             {
                 throw new InvalidMoveException();
             }
+
             base.Move(Square);
+
+            if (firstMove) firstMove = false;
         }
     }
 }
--- ChessEngine/tests/PawnTests.cs
+++ ChessEngine/tests/PawnTests.cs
@@ -2,6 +2,7 @@ using System;
 using ChessEngine.Common;
 using ChessEngine.Exceptions;
 using ChessEngine.Interfaces;
+using FluentAssertions;
 using Moq;
 using Xunit;
 
@@ -12,6 +13,7 @@ namespace ChessEngine.tests
         private Mock<ISquare> mockCurrentSquare;
         private Mock<IPlayer> mockPlayer;
         private Mock<ISquare> mockNewSquare;
+        private Pawn pawn;
 
         public PawnTests()
         {
@@ -20,6 +22,9 @@ namespace ChessEngine.tests
             mockNewSquare = new Mock<ISquare>();
 
             mockCurrentSquare.Setup(s => s.Position).Returns(("b", "2"));
+
+            pawn = new Pawn(mockCurrentSquare.Object, mockPlayer.Object);
+            pawn.TurnHandler += MockTurnEventListener;
         }
 
         private void MockTurnEventListener(object sender, TurnEventArgs e)
@@ -27,24 +32,43 @@ namespace ChessEngine.tests
         }
 
         [Fact]
-        public void Move_WhenPawnMoveRank_ShouldThrowInvalidMoveException()
+        public void Pawn_WhenMoveRank_ShouldThrowInvalidMoveException()
         {
             mockNewSquare.Setup(s => s.Position).Returns(("a", "2"));
             mockPlayer.SetupProperty(p => p.Turn, true);
 
-            var pawn = new Pawn(mockCurrentSquare.Object, mockPlayer.Object);
-            pawn.TurnHandler += MockTurnEventListener;
-
             Assert.Throws<InvalidMoveException>(() => pawn.Move(mockNewSquare.Object));
         }
         [Fact]
-        public void Move_WhenPawnMoveFileBack_ShouldThrowInvalidMoveException()
+        public void Pawn_WhenMoveFileBack_ShouldThrowInvalidMoveException()
         {
             mockNewSquare.Setup(s => s.Position).Returns(("b", "1"));
             mockPlayer.SetupProperty(p => p.Turn, true);
 
-            var pawn = new Pawn(mockCurrentSquare.Object, mockPlayer.Object);
-            pawn.TurnHandler += MockTurnEventListener;
+            Assert.Throws<InvalidMoveException>(() => pawn.Move(mockNewSquare.Object));
+        }
+        [Fact]
+        public void Pawn_AfterFirstMove_FirstMoveShouldBeFalse()
+        {
+            mockNewSquare.Setup(s => s.Position).Returns(("b", "3"));
+            mockPlayer.SetupProperty(p => p.Turn, true);
+
+            pawn.FirstMove.Should().BeTrue();
+
+            pawn.Move(mockNewSquare.Object);
+
+            pawn.FirstMove.Should().BeFalse();
+        }
+        [Fact]
+        public void Pawn_WhenFirstMoveFalse_MoveTwoFileShouldThrowInvalidMoveException()
+        {
+            mockNewSquare.Setup(s => s.Position).Returns(("b", "3"));
+            mockPlayer.SetupProperty(p => p.Turn, true);
+
+            pawn.Move(mockNewSquare.Object);
+
+            mockNewSquare.Setup(s => s.Position).Returns(("b", "5"));
+            mockPlayer.SetupProperty(p => p.Turn, true);
 
             Assert.Throws<InvalidMoveException>(() => pawn.Move(mockNewSquare.Object));
         }
