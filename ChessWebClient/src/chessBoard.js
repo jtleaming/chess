d7edf89e48af881e26c42ef8c75ccd20a6af78e4
Justin <dev@clouddev.test>
2019-05-13 13:14:10 -0600

    Completed chessboard test for web client.

--- ChessWebClient/src/chessBoard.js
+++ ChessWebClient/src/chessBoard.js
@@ -2,13 +2,22 @@ class chessBoard {
     constructor(boardString){
         this.board = document.createElement('div');
         this.board.className = 'chessboard';
-        this.chessPieces = {
+        this.chessPiecesBlack = {
             'rook': '&#9820;',
             'knight': '&#9822;',
             'bishop': '&#9821;',
             'king': '&#9819;',
             'queen': '&#9818',
-            'pawn': '&#9821;',
+            'pawn': '&#9823;',
+            '': ''
+        };
+        this.chessPiecesWhite = {
+            'rook': '&#9814;',
+            'knight': '&#9816;',
+            'bishop': '&#9815;',
+            'king': '&#9813;',
+            'queen': '&#9812;',
+            'pawn': '&#9817;',
             '': ''
         }
 
@@ -16,15 +25,40 @@ class chessBoard {
     }
 
     createBoard(boardString) {
-        let squares = JSON.parse(boardString);
-        let rows = ['a','b','c','d','e','f','g','h'];
-        for (let index = 0; index < 8; index++) {
-            let row = document.createElement('div');
-            row.id = `row-${index}`;
-            // rows.push(row);
-            for (let a = 0; a < rows.length; a++) {
-                let squareId = `${rows[a]}${a+1}`
-                let square = document.createElement('div');
+        let squares = Object.entries(JSON.parse(boardString));
+        var first = 'black';
+        var second = 'white';
+        for (let index = 57; index >= 1; index++) {
+            let square = squares[index-1];
+            let squareId = square[0];
+            let div = document.createElement('div');
+            div.id = squareId;
+            if(squareId[1] % 2 === 1) {
+                first = 'black';
+                second = 'white';
+            } else {
+                first = 'white';
+                second = 'black';
+            }
+
+            if(squareId[0].charCodeAt(0) % 2 === 1) {
+                div.className = first;
+            } else {
+                div.className = second;
+            }
+
+            if(square[1].Occupied){
+                if(squareId[1] === '1' || squareId[1] === '2') {
+                    div.innerHTML = this.chessPiecesWhite[square[1].Piece.Type.toLowerCase()];
+                } else {
+                    div.innerHTML = this.chessPiecesBlack[square[1].Piece.Type.toLowerCase()];
+                }
+            }
+
+            this.board.appendChild(div);
+
+            if (index % 8 === 0) {
+                index -= 16;
             }
         }
         var body = document.getElementsByTagName('body')[0];
