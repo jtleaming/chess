8c982074a2b21a2dafa0fae59ea54627aeac942b
Justin <dev@clouddev.test>
2019-05-13 13:32:10 -0600

    Refactoring coloring grid.

--- ChessWebClient/src/chessBoard.js
+++ ChessWebClient/src/chessBoard.js
@@ -2,56 +2,47 @@ class chessBoard {
     constructor(boardString){
         this.board = document.createElement('div');
         this.board.className = 'chessboard';
-        this.chessPiecesBlack = {
-            'rook': '&#9820;',
-            'knight': '&#9822;',
-            'bishop': '&#9821;',
-            'king': '&#9819;',
-            'queen': '&#9818',
-            'pawn': '&#9823;',
-            '': ''
+        this.chessPieces = {
+            black: {
+                'rook': '&#9820;',
+                'knight': '&#9822;',
+                'bishop': '&#9821;',
+                'king': '&#9819;',
+                'queen': '&#9818',
+                'pawn': '&#9823;'
+            },
+            white: {
+                'rook': '&#9814;',
+                'knight': '&#9816;',
+                'bishop': '&#9815;',
+                'king': '&#9813;',
+                'queen': '&#9812;',
+                'pawn': '&#9817;'
+            }
         };
-        this.chessPiecesWhite = {
-            'rook': '&#9814;',
-            'knight': '&#9816;',
-            'bishop': '&#9815;',
-            'king': '&#9813;',
-            'queen': '&#9812;',
-            'pawn': '&#9817;',
-            '': ''
-        }
 
         this.createBoard(boardString);
     }
 
     createBoard(boardString) {
         let squares = Object.entries(JSON.parse(boardString));
-        var first = 'black';
-        var second = 'white';
         for (let index = 57; index >= 1; index++) {
             let square = squares[index-1];
             let squareId = square[0];
             let div = document.createElement('div');
             div.id = squareId;
-            if(squareId[1] % 2 === 1) {
-                first = 'black';
-                second = 'white';
-            } else {
-                first = 'white';
-                second = 'black';
-            }
 
-            if(squareId[0].charCodeAt(0) % 2 === 1) {
-                div.className = first;
+            if((squareId[0].charCodeAt() + parseInt(squareId[1])) % 2 === 0) {
+                div.className = 'black';
             } else {
-                div.className = second;
+                div.className = 'white';
             }
 
             if(square[1].Occupied){
                 if(squareId[1] === '1' || squareId[1] === '2') {
-                    div.innerHTML = this.chessPiecesWhite[square[1].Piece.Type.toLowerCase()];
+                    div.innerHTML = this.chessPieces.white[square[1].Piece.Type.toLowerCase()];
                 } else {
-                    div.innerHTML = this.chessPiecesBlack[square[1].Piece.Type.toLowerCase()];
+                    div.innerHTML = this.chessPieces.black[square[1].Piece.Type.toLowerCase()];
                 }
             }
 
